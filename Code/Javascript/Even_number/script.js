481bacdffb132b0984f75af511efb88cccd26c35
Thiago Moraes <thiago4x70@gmail.com>
2018-09-24 00:23:56 -0300

    Improving the code of the isEven exercise

--- Code/Javascript/Even_number/script.js
+++ Code/Javascript/Even_number/script.js
@@ -3,12 +3,17 @@
     and returns true if the number is even, and false otherwise
 */
 
-function isEven (num){
-    if(num % 2 === 0) {
-      return true;
-    } else {
-      return false;
-    }
+//function isEven (num){
+//    if(num % 2 === 0) {
+//      return true;
+//    } else {
+//      return false;
+//    }
+//}
+
+// Much shorter code
+function isEven(num){
+	return num % 2 === 0;
 }
 
 while(n !== 0){
