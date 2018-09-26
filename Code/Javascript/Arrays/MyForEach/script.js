d4f13047f7fff2c0d6ac45fa10188216bf7434b8
Thiago Moraes <thiago4x70@gmail.com>
2018-09-26 13:05:34 -0300

    Creating my own forEach function

---
+++ Code/Javascript/Arrays/MyForEach/script.js
@@ -0,0 +1,28 @@
+
+var colors = ["red", "yellow", "orange"];
+
+// Creating a forEach function
+function myForEach(arr, func) {
+  //  loop through array
+  for(var i = 0; i < arr.length; i++) {
+    //  Call function for each item in the array
+    func(arr[i]);
+  }
+}
+
+myForEach(colors, function(color){
+  console.log(color);
+});
+
+
+// Creating a forEach Object
+Array.prototype.myOtherForEach = function(func){
+  for(var i = 0; i < this.length; i++){
+    func(this[i]);
+  }
+}
+
+console.log("******************\nOther function\n******************");
+colors.myOtherForEach(function(el){
+  console.log(el);
+});
