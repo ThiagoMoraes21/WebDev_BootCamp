784a2ba2ec7d96eaf2ce3dcade94e52b6ad77182
Thiago Moraes <thiago4x70@gmail.com>
2018-09-26 10:10:25 -0300

    Improving a little the todo console app

--- Code/Javascript/ToDo_App_back-end/list.js
+++ Code/Javascript/ToDo_App_back-end/list.js
@@ -1,23 +1,40 @@
-var toDos = ["something useless"];
+var toDos = ["Do something productive!"];
 
 window.setTimeout(function(){
   while(input !== "quit"){
     var input = prompt("What do you like to do?");
 
     if (input === "new") {
-      var newToDo = prompt("Enter a new task!");
-      toDos.push(newToDo);
-
+      addToDos();
     } else if (input === "list") {
-      return listTasks(toDos);
-
-    } else {
-      alert("This option is not available!");
+      listToDos();
+    } else if(input === "delete") {
+      deleteToDos();
     }
   }
+  console.log("OK, YOU QUIT THE APP");
 
-  function listTasks(tasks){
-    console.log(toDos);
-    document.getElementById('todo').innerHTML = tasks;
+
+  function listToDos(){
+    console.log("******************************");
+    toDos.forEach(function(task, index){
+    console.log(index + ": " + task);
+    });
+    console.log("******************************");
   }
+
+  function addToDos(){
+    var newToDo = prompt("Enter a new task!");
+    toDos.push(newToDo);
+    console.log("New To Do added");
+  }
+
+  function deleteToDos(){
+    // ask for the index of the element to be delete
+    var index = prompt("Enter the index of todo to be delete");
+    // delete the choseen element
+    toDos.splice(index, 1);
+    console.log("Deleted To Do");
+  }
+
 }, 500);
