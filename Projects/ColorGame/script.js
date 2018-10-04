6d6783a1246ae59fb063923a2207a49ab961fd28
Thiago Moraes <thiago4x70@gmail.com>
2018-10-04 00:35:44 -0300

    ColorGAme:  Adding new fictures to the project

--- Projects/ColorGame/script.js
+++ Projects/ColorGame/script.js
@@ -5,6 +5,27 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetBtn = document.getElementById("reset");
+
+resetBtn.addEventListener("click", function(){
+  // generate all new colors
+  colors = generateRandomColors(6);
+  // pick a new random color from the array
+  pickedColor = pickColor();
+  // change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  // change the colors of squares
+  for(var i = 0; i < squares.length; i ++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  // reset the h1 backgroundColor
+  h1.style.backgroundColor = "#232323";
+  // reset the message
+  messageDisplay.textContent = "";
+  // reset the btn text content
+  resetBtn.textContent = "New Colors";
+});
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
@@ -20,6 +41,7 @@ for(var i = 0; i < squares.length; i++) {
     messageDisplay.textContent = "Correct!";
     changeColors(clickedColor);
     h1.style.backgroundColor = clickedColor;
+    resetBtn.textContent = "Play Again?";
   } else {
     messageDisplay.textContent = "Try Again!";
     this.style.backgroundColor = "#232323";
