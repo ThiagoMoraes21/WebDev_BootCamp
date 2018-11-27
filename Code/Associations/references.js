bfdfe20d9ef5198cd56476e86f87091c3c8c928f
Thiago Moraes <thiago4x70@gmail.com>
2018-11-27 09:35:32 -0200

    Add new doc

--- Code/Associations/references.js
+++ Code/Associations/references.js
@@ -5,37 +5,37 @@ var Post = require('./models/post');
 var User = require('./models/user');
 
 //  create and save a new user to the DB
-// User.create({
-//   email: "bob@gmail.com",
-//   name: "Bob Belcher"
-// });
+User.create({
+  email: "bob@gmail.com",
+  name: "Bob Belcher"
+});
 
-// create a new post
-// Post.create({
-//   title: "How to cock the best burguer pt.3",
-//   content: "And to finish and give this recipe a nice touch let's do..."
-// }, function(err, post){
-//     if (err) {
-//       console.log("Create user error\n" + err);
-//     } else {
-//       // find the user
-//       User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-//         if(err){
-//           console.log("Finding user error\n" + err);
-//         } else {
-//           //  take the post and push into the user posts array
-//           foundUser.posts.push(post);
-//           foundUser.save(function(err, data){ // save the user
-//             if (err) {
-//               console.log("Saving post error\n" + err);
-//             } else {
-//               console.log(data); // print out the saved post
-//             }
-//           });
-//         }
-//       });
-//     }
-// });
+//  create a new post
+Post.create({
+  title: "How to cock the best burguer pt.3",
+  content: "And to finish and give this recipe a nice touch let's do..."
+}, function(err, post){
+    if (err) {
+      console.log("Create user error\n" + err);
+    } else {
+      // find the user
+      User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
+        if(err){
+          console.log("Finding user error\n" + err);
+        } else {
+          //  take the post and push into the user posts array
+          foundUser.posts.push(post);
+          foundUser.save(function(err, data){ // save the user
+            if (err) {
+              console.log("Saving post error\n" + err);
+            } else {
+              console.log(data); // print out the saved post
+            }
+          });
+        }
+      });
+    }
+});
 
 //  Query to retrive the data and fild in
 //  the post array with all the data not only the Id's
