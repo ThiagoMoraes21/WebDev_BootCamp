61cb7324dcfd1351d5c17cde696e8d15d81a5e72
Thiago Moraes <thiago4x70@gmail.com>
2018-11-28 11:52:52 -0200

    Configure passport, add register routes and template

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -3,6 +3,9 @@ var express = require('express'),
     port  = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    User = require('./models/user'),
     Campground = require('./models/campground'),
     seedDB = require('./seeds'),
     Comment   = require("./models/comment");
@@ -11,6 +14,16 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(require('express-session')({  // passport config
+  secret: 'Bob is the best dog of the world!',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 seedDB(); // remove existent campgrounds and recreate them
 
 app.get("/", function(req, res){
@@ -101,6 +114,30 @@ app.post("/campgrounds/:id/comments", function(req, res){
     }
   });
 });
+
+// ====================
+//  AUTH ROUTES
+// ====================
+
+//  show register form
+app.get('/register', function(req, res){
+  res.render('register');
+});
+
+//  handle sign up logic
+app.post('/register', function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if (err) {
+      console.log('Error on register a new user\n' + err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, function(){
+      res.redirect('/campgrounds');
+    });
+  });
+});
+
 
 // ERROR 404 PAGE NOT FOUND
 app.get("*", function(req, res){
