08c45473351ac2536d0bb053580bd0fce59281d3
Thiago Moraes <thiago4x70@gmail.com>
2018-12-01 18:31:24 -0200

    User association with created campgrounds

--- Projects/YelpCamp/routes/campgrounds.js
+++ Projects/YelpCamp/routes/campgrounds.js
@@ -20,25 +20,36 @@ router.get("/", function(req, res){
 });
 
 // CREATE - Add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
   // get the data from the form
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc}
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  var newCampground = {
+    name: name,
+    image: image,
+    description: desc,
+    author: author
+  }
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, createdCampground){
     if(err){
-      console.log(err);
+      console.log('Create new campground failed: \n' + err);
+      res.redirect('/campgrounds/new');
     } else {
       // redirect back to campgrounds page
-      res.redirect("/campgrounds");
+      console.log(createdCampground);
+      res.redirect("/campgrounds/" + createdCampground._id);
     }
   });
 });
 
 // NEW - Show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
   res.render("campgrounds/new.ejs");
 });
 
@@ -54,5 +65,13 @@ router.get("/:id", function(req, res){
     }
   });
 });
+
+//  Middleware that checks user authentication
+function isLoggedIn(req, res, next){
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 module.exports = router;
