bb7935bb74b51dac08ff522095c3f2e3d55d653d
Thiago Moraes <thiago4x70@gmail.com>
2018-12-04 20:54:16 -0200

    Add comments authorization

--- Projects/YelpCamp/routes/comments.js
+++ Projects/YelpCamp/routes/comments.js
@@ -43,7 +43,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //  COMMENTS EDIT ROUTE
-router.get('/:comment_id/edit', function(req, res){
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res){
   Comment.findById(req.params.comment_id, function(err, foundComment){
     if (err) {
       console.log('Error trying to find the comment: \n' + err);
@@ -58,7 +58,7 @@ router.get('/:comment_id/edit', function(req, res){
 });
 
 //  COMMENTS UPDATE ROUTE
-router.put('/:comment_id', function(req, res){
+router.put('/:comment_id', checkCommentOwnership, function(req, res){
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment,
     function(err, updatedComment){
       if (err) {
@@ -71,7 +71,7 @@ router.put('/:comment_id', function(req, res){
 });
 
 //  COMMENT DESTROY ROUTE
-router.delete('/:comment_id', function(req, res){
+router.delete('/:comment_id', checkCommentOwnership, function(req, res){
   Comment.findByIdAndRemove(req.params.comment_id, function(err){
      if(err){
         console.log('Error trying to delete comment: \n' + err);
@@ -81,6 +81,27 @@ router.delete('/:comment_id', function(req, res){
      }
   });
 });
+
+//  Middleware that checks the comment ownership
+function checkCommentOwnership(req, res, next){
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if (err) {
+        console.log('Error trying to find the campground' + err);
+        res.redirect('back');
+      } else {
+        //  check if the user own the comment
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+}
 
 //  Middleware that checks user authentication
 function isLoggedIn(req, res, next){
