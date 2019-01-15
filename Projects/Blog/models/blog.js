c45173def95a5c7d51c2416743d552c61769f686
Thiago Moraes <thiago4x70@gmail.com>
2019-01-15 21:34:54 -0200

    Style the post page

--- Projects/Blog/models/blog.js
+++ Projects/Blog/models/blog.js
@@ -3,6 +3,7 @@ let mongoose = require('mongoose');
 //  Blog mondel
 var blogSchema = new mongoose.Schema({
   title: String,
+  subTitle: String,
   image: String,
   body: String,
   created: {type: Date, default: Date.now}
